73d7041527b8cc46ed3868756cda978408b3dc9f
Sergey Khaladzinski <gansbrest@yahoo.com>
2013-05-07 14:23:46 -0400

    Added Metric namespacing support  https://github.com/etsy/statsd/blob/master/docs/namespacing.md

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,4 +7,11 @@ default["statsd"]["address"]                      = "0.0.0.0"
 default["statsd"]["port"]                         = 8125
 default["statsd"]["graphite_host"]                = "localhost"
 default["statsd"]["graphite_port"]                = 2003
+
+# Graphite storage config
 default["statsd"]["graphite"]["legacy_namespace"] = true
+default["statsd"]["graphite"]["global_prefix"] = "stats"
+default["statsd"]["graphite"]["prefix_counter"] = "counters"
+default["statsd"]["graphite"]["prefix_timer"] = "timers"
+default["statsd"]["graphite"]["prefix_gauge"] = "gauges"
+default["statsd"]["graphite"]["prefix_set"] = "sets"

92babe5dc766ad1472958a0e3198a8fd77e23c5c
Chris Ferry <cferry@secondmarket.com>
2013-06-18 17:43:48 -0400

    Add ability to search for graphite host based on graphite_query attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,8 +5,10 @@ default["statsd"]["log_file"]                     = "/var/log/statsd.log"
 default["statsd"]["flush_interval"]               = 10000
 default["statsd"]["address"]                      = "0.0.0.0"
 default["statsd"]["port"]                         = 8125
-default["statsd"]["graphite_host"]                = "localhost"
+default["statsd"]["graphite_host"]                = "127.0.0.1"
 default["statsd"]["graphite_port"]                = 2003
+default['statsd']['graphite_role']                = 'graphite_server'
+default['statsd']['graphite_query']               = "roles:#{node['statsd']['graphite_role']} AND chef_environment:#{node.chef_environment}"
 default["statsd"]["delete_timers"]                = false
 default["statsd"]["delete_gauges"]                = false
 
